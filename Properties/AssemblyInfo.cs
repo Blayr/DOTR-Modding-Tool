81e2a16136e64fd92b45bf81d0e818081ce6d2ab
Blayr <BlairMichaud@Gmail.com>
2020-08-29 14:47:12 -0300

    Update project name in AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DOTR Deck Leader Thresholds")]
+[assembly: AssemblyTitle("DOTR Modding Tool")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DOTR Deck Leader Thresholds")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyProduct("DOTR Modding Tool")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
