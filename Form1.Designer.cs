8dede4e2e3aaa7b2cbe3926a81a46504f07c73ab
Blayr <BlairMichaud@Gmail.com>
2020-08-29 14:52:33 -0300

    Show dialog in center of screen on start

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -194,6 +194,7 @@
       this.Controls.Add(this.menuStrip1);
       this.MainMenuStrip = this.menuStrip1;
       this.Name = "MainForm";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
       this.Text = "DOTR Modding Tool";
       this.Load += new System.EventHandler(this.Form1_Load);
       this.menuStrip1.ResumeLayout(false);
