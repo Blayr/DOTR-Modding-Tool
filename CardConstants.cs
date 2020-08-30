229406122b9f51f611b2c7df5186aa3b89af470c
Blayr <BlairMichaud@Gmail.com>
2020-08-30 15:41:34 -0300

    Populate more CardConstant fields/properties

--- CardConstants.cs
+++ CardConstants.cs
@@ -1,6 +1,10 @@
 ﻿using DOTR_MODDING_TOOL.Properties;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO.Ports;
+using System.Security.Policy;
+using System.Text;
 
 public class CardConstants
 {
@@ -27,19 +31,21 @@ public class CardConstant
 	byte deckCost;
 	ushort effectId;
 	ushort xaxId;
-
-	//// u8 kind;
-	//// u8 kindOfs;
-	//// u8 lvAttr;
-	//// u8 deckCost;
-	//// u16 effectId;
-	//// u16 xaxId;
-	//// u16 apWithFlags;
-	//// u16 dpWithFlags;
+	BitArray apWithFlags;
+	ushort attack;
+	bool hasAlternateArt;
+	bool isSlotRare;
+	bool appearsInSlotReels;
+	BitArray dpWithFlags;
+	ushort defense;
+	bool hasImage;
+	bool passwordWorks;
+	bool appearsInReincarnation;
 
 	public CardConstant(ushort cardIndex, byte[] bytes)
   {
 		this.cardIndex = cardIndex;
+		this.name = Cards.GetNameByIndex(cardIndex);
 		this.bytes = bytes;
 		this.kind = bytes[0];
 		this.cardKind = new CardKind(this.kind);
@@ -47,8 +53,30 @@ public class CardConstant
 		this.lvAttr = bytes[2];
 		this.deckCost = bytes[3];
 		this.effectId = BitConverter.ToUInt16(new byte[] { bytes[4], bytes[5] }, 0);
-		this.xaxId = BitConverter.ToUInt16(new byte[] { bytes[4], bytes[5] }, 0);
-		this.name = Cards.GetNameByIndex(cardIndex);
+		this.xaxId = BitConverter.ToUInt16(new byte[] { bytes[6], bytes[7] }, 0);
+
+		this.apWithFlags = new BitArray(new byte[] { bytes[8], bytes[9] });
+		this.attack = CardConstant.GetAttackOrDefense(new byte[] { bytes[8], bytes[9] });
+		this.hasImage = apWithFlags[apWithFlags.Length - 3];
+		this.passwordWorks = apWithFlags[apWithFlags.Length - 2];
+		this.appearsInReincarnation = apWithFlags[apWithFlags.Length - 1];
+
+		this.dpWithFlags = new BitArray(new byte[] { bytes[10], bytes[11] });
+		this.defense = CardConstant.GetAttackOrDefense(new byte[] { bytes[10], bytes[11] });
+		this.appearsInSlotReels = dpWithFlags[dpWithFlags.Length - 3];
+		this.isSlotRare = dpWithFlags[dpWithFlags.Length - 2];
+		this.hasAlternateArt = dpWithFlags[dpWithFlags.Length - 1];
+	}
+
+	public static ushort GetAttackOrDefense(byte[] bytes)
+  {
+		BitArray bitArray = new BitArray(bytes);
+		bitArray[bitArray.Length - 1] = false;
+		bitArray[bitArray.Length - 2] = false;
+		bitArray[bitArray.Length - 3] = false;
+		byte[] newBytes = new byte[2];
+		bitArray.CopyTo(newBytes, 0);
+		return BitConverter.ToUInt16(newBytes, 0);
 	}
 
 	public ushort Index
@@ -75,14 +103,6 @@ public class CardConstant
     }
   }
 
-	public byte Kind
-  {
-    get
-    {
-			return this.kind;
-    }
-  }
-
 	public string KindName
   {
     get
@@ -96,6 +116,70 @@ public class CardConstant
     get
     {
 			return this.effectId;
+    }
+  }
+
+	public ushort Attack
+  {
+    get
+    {
+			return this.attack;
+    }
+  }
+
+	public ushort Defense
+  {
+    get
+    {
+			return this.defense;
+    }
+  }
+
+	public bool IsSlotRare
+  {
+    get
+    {
+			return this.isSlotRare;
+    }
+  }
+
+	public bool AppearsInSlotReels
+  {
+    get
+    {
+			return this.appearsInSlotReels;
+    }
+  }
+
+	public bool AppearsInReincarnation
+  {
+    get
+    {
+			return this.appearsInReincarnation;
+    }
+  }
+
+	public bool PasswordWorks
+  {
+    get
+    {
+			return this.passwordWorks;
+    }
+  }
+
+	public bool HasImage
+  {
+    get
+    {
+			return this.hasImage;
+    }
+  }
+
+	public bool HasAlternateArt
+  {
+    get
+    {
+			return this.hasAlternateArt;
     }
   }
 }
