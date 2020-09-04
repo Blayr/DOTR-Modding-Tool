a91242cbbdb2c55c952f734ef8eff0e87092b06f
Blayr <BlairMichaud@Gmail.com>
2020-09-03 22:38:20 -0300

    Remove debug code that was causing slot rare flag to be set to true

--- CardConstants.cs
+++ CardConstants.cs
@@ -76,8 +76,6 @@ public class CardConstant
 		this.appearsInSlotReels = dpWithFlags[dpWithFlags.Length - 3];
 		this.isSlotRare = dpWithFlags[dpWithFlags.Length - 2];
 
-		this.dpWithFlags[dpWithFlags.Length - 2] = true;
-
 		this.hasAlternateArt = dpWithFlags[dpWithFlags.Length - 1];
 		this.passwordArray = new byte[] { bytes[12], bytes[13], bytes[14], bytes[15], bytes[16], bytes[17], bytes[18], bytes[19] };
 	}
