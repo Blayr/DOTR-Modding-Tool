1267457850238d31de1c32987670c15ef2a52bb8
Blayr <BlairMichaud@Gmail.com>
2020-09-12 19:51:46 -0300

    Create DebugHelper class to speed up debugging process

---
+++ DebugHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+public static class DebugHelper
+{
+	static DebugHelper()
+	{
+	}
+
+	public static void PrintBitArray(BitArray bitArray, int groupSize)
+	{
+		string outputString = "";
+
+		for (int i = 0; i < bitArray.Length; i++)
+    {
+			if (i % groupSize == 0)
+      {
+				outputString += " ";
+      }
+
+			outputString += bitArray[i] ? "1" : "0";
+    }
+
+		System.Diagnostics.Debug.WriteLine(outputString);
+  }
+
+	public static void PrintByteArray(byte[] byteArray)
+  {
+		System.Diagnostics.Debug.Print(BitConverter.ToString(byteArray));
+  }
+}
