ad727c26186fb1095e34584ecb7c3b45d0039a96
Blayr <BlairMichaud@Gmail.com>
2020-09-25 00:07:16 -0300

    Move DeckLeaderThreshold form code into its own class

---
+++ Forms/DeckLeaderThresholdsForm.cs
@@ -0,0 +1,100 @@
+﻿namespace DOTR_Modding_Tool
+{
+  using Equin.ApplicationFramework;
+  using System;
+  using System.Collections.Generic;
+  using System.ComponentModel;
+  using System.Diagnostics;
+  using System.Drawing;
+  using System.Linq;
+  using System.Runtime.CompilerServices;
+  using System.Windows.Forms;
+
+  public partial class MainForm : Form
+  {
+    private DeckLeaderRankThresholds originalDeckLeaderRankThresholds;
+    private DeckLeaderRankThresholds newDeckLeaderRankThresholds;
+
+    private void RankThresholdDataGridView_CellValueChanged(
+    object sender, DataGridViewCellEventArgs e)
+    {
+      this.DisplayThresholdBytes(this.newRankThresholdsTextbox, this.newDeckLeaderRankThresholds.Bytes);
+    }
+
+    private void LoadLeaderTresholdData()
+    {
+      byte[] byteData = this.dataAccess.LoadLeaderTresholdData();
+      this.originalDeckLeaderRankThresholds = new DeckLeaderRankThresholds(byteData);
+      this.newDeckLeaderRankThresholds = new DeckLeaderRankThresholds(byteData);
+
+      this.DisplayThresholdBytes(this.originalRankThresholdsTextbox, this.originalDeckLeaderRankThresholds.Bytes);
+      this.rankThresholdsDataGridView.DataSource = this.newDeckLeaderRankThresholds.Thresholds;
+    }
+
+    private void Button1_Click(object sender, EventArgs e)
+    {
+      this.dataAccess.SetDeckLeaderTresholds(this.newDeckLeaderRankThresholds.Bytes);
+      this.LoadLeaderTresholdData();
+    }
+
+    private void SetupRankThresholdDataGridView()
+    {
+      this.rankThresholdsDataGridView.AutoGenerateColumns = false;
+      DataGridViewColumn rankImageColumn = new DataGridViewImageColumn();
+      rankImageColumn.DataPropertyName = "RankImage";
+      rankImageColumn.HeaderText = string.Empty;
+      rankImageColumn.Name = "RankImage";
+      rankImageColumn.ReadOnly = true;
+      rankImageColumn.Width = 30;
+      this.rankThresholdsDataGridView.Columns.Add(rankImageColumn);
+      DataGridViewColumn rankNameColumn = new DataGridViewTextBoxColumn();
+      rankNameColumn.DataPropertyName = "RankName";
+      rankNameColumn.HeaderText = "Rank";
+      rankNameColumn.Name = "RankName";
+      rankNameColumn.ReadOnly = true;
+      this.rankThresholdsDataGridView.Columns.Add(rankNameColumn);
+      DataGridViewColumn rankThresholdColumn = new DataGridViewTextBoxColumn();
+      rankThresholdColumn.DataPropertyName = "Threshold";
+      rankThresholdColumn.HeaderText = "Threshold";
+      rankThresholdColumn.Name = "Threshold";
+      rankThresholdColumn.ReadOnly = false;
+      this.rankThresholdsDataGridView.Columns.Add(rankThresholdColumn);
+      this.rankThresholdsDataGridView.CellValueChanged += this.RankThresholdDataGridView_CellValueChanged;
+      this.rankThresholdsDataGridView.DataError += this.CorrectInvalidRankThreshold;
+    }
+
+    private void CorrectInvalidRankThreshold(object sender, DataGridViewDataErrorEventArgs e)
+    {
+      DataGridViewCell cell = this.rankThresholdsDataGridView[e.ColumnIndex, e.RowIndex];
+      string cellValue = (string)cell.EditedFormattedValue;
+
+      ushort newThresholdValue;
+      int inputtedNumber;
+
+      if (!Int32.TryParse(cellValue, out inputtedNumber))
+      {
+        newThresholdValue = (ushort)cell.Value;
+      }
+      else if (inputtedNumber < ushort.MinValue)
+      {
+        newThresholdValue = ushort.MinValue;
+      }
+      else if (inputtedNumber > ushort.MaxValue)
+      {
+        newThresholdValue = ushort.MaxValue;
+      }
+      else
+      {
+        newThresholdValue = (ushort)cell.Value;
+      }
+
+      cell.Value = newThresholdValue;
+      e.Cancel = false;
+    }
+
+    private void DisplayThresholdBytes(TextBox textbox, byte[] bytes)
+    {
+      textbox.Text = BitConverter.ToString(bytes);
+    }
+  }
+}
--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -13,8 +13,6 @@
   public partial class MainForm : Form
   {
     private DataAccess dataAccess = new DataAccess();
-    private DeckLeaderRankThresholds originalDeckLeaderRankThresholds;
-    private DeckLeaderRankThresholds newDeckLeaderRankThresholds;
     private CardConstants cardConstants;
     private BindingListView<CardConstant> cardConstantsBinding;
     private Fusions fusions;
@@ -112,71 +110,6 @@
       bro.Rows[e.RowIndex].DefaultCellStyle.ForeColor = Color.White;
     }
 
-    private void SetupRankThresholdDataGridView()
-    {
-      this.rankThresholdsDataGridView.AutoGenerateColumns = false;
-      DataGridViewColumn rankImageColumn = new DataGridViewImageColumn();
-      rankImageColumn.DataPropertyName = "RankImage";
-      rankImageColumn.HeaderText = string.Empty;
-      rankImageColumn.Name = "RankImage";
-      rankImageColumn.ReadOnly = true;
-      rankImageColumn.Width = 30;
-      this.rankThresholdsDataGridView.Columns.Add(rankImageColumn);
-      DataGridViewColumn rankNameColumn = new DataGridViewTextBoxColumn();
-      rankNameColumn.DataPropertyName = "RankName";
-      rankNameColumn.HeaderText = "Rank";
-      rankNameColumn.Name = "RankName";
-      rankNameColumn.ReadOnly = true;
-      this.rankThresholdsDataGridView.Columns.Add(rankNameColumn);
-      DataGridViewColumn rankThresholdColumn = new DataGridViewTextBoxColumn();
-      rankThresholdColumn.DataPropertyName = "Threshold";
-      rankThresholdColumn.HeaderText = "Threshold";
-      rankThresholdColumn.Name = "Threshold";
-      rankThresholdColumn.ReadOnly = false;
-      this.rankThresholdsDataGridView.Columns.Add(rankThresholdColumn);
-      this.rankThresholdsDataGridView.CellValueChanged += this.RankThresholdDataGridView_CellValueChanged;
-      this.rankThresholdsDataGridView.DataError += this.CorrectInvalidRankThreshold;
-    }
-
-    private void CorrectInvalidRankThreshold(object sender, DataGridViewDataErrorEventArgs e)
-    {
-      DataGridViewCell cell = this.rankThresholdsDataGridView[e.ColumnIndex, e.RowIndex];
-      string cellValue = (string)cell.EditedFormattedValue;
-
-      ushort newThresholdValue;
-      int inputtedNumber;
-
-      if (!Int32.TryParse(cellValue, out inputtedNumber))
-      {
-        newThresholdValue = (ushort)cell.Value;
-      }
-      else if (inputtedNumber < ushort.MinValue)
-      {
-        newThresholdValue = ushort.MinValue;
-      }
-      else if (inputtedNumber > ushort.MaxValue)
-      {
-        newThresholdValue = ushort.MaxValue;
-      } else
-      {
-        newThresholdValue = (ushort)cell.Value;
-      }
-
-      cell.Value = newThresholdValue;
-      e.Cancel = false;
-    }
-
-    private void RankThresholdDataGridView_CellValueChanged(
-    object sender, DataGridViewCellEventArgs e)
-    {
-      this.DisplayThresholdBytes(this.newRankThresholdsTextbox, this.newDeckLeaderRankThresholds.Bytes);
-    }
-
-    private void DisplayThresholdBytes(TextBox textbox, byte[] bytes)
-    {
-      textbox.Text = BitConverter.ToString(bytes);
-    }
-
     private void FileToolStripMenuItem_Click(object sender, EventArgs e)
     {
       this.OpenSelectISODialog();
@@ -189,7 +122,6 @@
         return;
       }
 
-      originalRankThresholdsTextbox.Text = openFileDialog1.FileName;
       this.dataAccess.OpenIso(openFileDialog1.FileName);
       this.LoadDataFromIso();
     }
@@ -319,21 +251,6 @@
       this.cardConstantsDataGridView.DefaultCellStyle.Font = new Font("OpenSans", 9.75F, FontStyle.Regular);
     }
 
-    private void LoadLeaderTresholdData()
-    {
-      byte[] byteData = this.dataAccess.LoadLeaderTresholdData();
-      this.originalDeckLeaderRankThresholds = new DeckLeaderRankThresholds(byteData);
-      this.newDeckLeaderRankThresholds = new DeckLeaderRankThresholds(byteData);
-
-      this.DisplayThresholdBytes(this.originalRankThresholdsTextbox, this.originalDeckLeaderRankThresholds.Bytes);
-      this.rankThresholdsDataGridView.DataSource = this.newDeckLeaderRankThresholds.Thresholds;
-    }
-
-    private void Button1_Click(object sender, EventArgs e)
-    {
-      this.dataAccess.SetDeckLeaderTresholds(this.newDeckLeaderRankThresholds.Bytes);
-      this.LoadLeaderTresholdData();
-    }
     private void saveToolStripMenuItem_Click(object sender, EventArgs e)
     {
       // Ensure the menustrip is focused so that the datagridview changes are commited
