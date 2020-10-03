2751b33ae1010afe55a90a149d436cd7ab925928
Blayr <BlairMichaud@Gmail.com>
2020-10-03 20:16:12 -0300

    Replace magic number and use auto properties where available in DeckLeaderRankThresholds

--- Classes/DeckLeaderRankThresholds.cs
+++ Classes/DeckLeaderRankThresholds.cs
@@ -6,7 +6,8 @@ using System.Linq;
 
 public class DeckLeaderRankThresholds
 {
-  private byte totalRanks = 12;
+  private readonly byte totalRanks = 12;
+  private readonly byte rankByteSize = 2;
   public List<DeckLeaderRankThreshold> Thresholds = new List<DeckLeaderRankThreshold> { };
 
   public DeckLeaderRankThresholds(byte[] byteData)
@@ -18,8 +19,7 @@ public class DeckLeaderRankThresholds
   {
     for (byte thresholdsIndex = 0; thresholdsIndex < totalRanks; thresholdsIndex++)
     {
-      int bytesIndex = thresholdsIndex * 2;
-      // Insert the bytes here in reverse order, as the word size is 4, and the PS2 processor architecture is little-endian
+      int bytesIndex = thresholdsIndex * rankByteSize;
       byte[] thresholdBytes = new byte[] { bytes[bytesIndex], bytes[bytesIndex + 1] };
       this.Thresholds.Add(new DeckLeaderRankThreshold(thresholdsIndex, thresholdBytes));
     }
@@ -69,9 +69,9 @@ public class DeckLeaderRankThreshold {
   public DeckLeaderRankThreshold(byte rankIndex, byte[] byteData)
   {
     this.Bytes = byteData;
-    this.rankIndex = rankIndex;
-    this.rankName = RankNames[this.rankIndex];
-    this.SetRankImage();
+    this.RankIndex = rankIndex;
+    this.RankName = RankNames[this.RankIndex];
+    this.RankImage = new Bitmap(RankImages[this.RankIndex], new Size(30, 30));
   }
 
   public void SetThresholdWithBytes(byte[] byteData)
@@ -79,16 +79,8 @@ public class DeckLeaderRankThreshold {
     this.threshold = BitConverter.ToUInt16(new byte[] { byteData[0], byteData[1] }, 0);
   }
 
-  public void SetRankImage()
-  {
-    this.rankImage = new Bitmap(RankImages[this.rankIndex], new Size(30, 30));
-  }
-
-  private byte rankIndex;
   private byte[] bytes;
   private ushort threshold;
-  private string rankName;
-  private Image rankImage;
 
   public byte[] Bytes
   {
@@ -118,19 +110,7 @@ public class DeckLeaderRankThreshold {
     }
   }
 
-  public string RankName
-  {
-    get
-    {
-      return this.rankName;
-    }
-  }
-
-  public Image RankImage
-  {
-    get
-    {
-      return this.rankImage;
-    }
-  }
+  public ushort RankIndex { get; }
+  public string RankName{ get; }
+  public Image RankImage { get;  }
 }
