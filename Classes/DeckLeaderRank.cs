b890a02f877d866fe5e2ee33ca165992c4038881
Blayr <BlairMichaud@Gmail.com>
2020-10-04 17:41:57 -0300

    Create DeckLeaderRank class and use it in DeckLeaderRankThreshold

---
+++ Classes/DeckLeaderRank.cs
@@ -0,0 +1,48 @@
+﻿using DOTR_MODDING_TOOL.Properties;
+using System;
+using System.Drawing;
+
+public class DeckLeaderRank
+{
+  public int Index { get; }
+  public string Name { get; }
+  public Image Image { get; }
+	public DeckLeaderRank(int index)
+	{
+    this.Index = index;
+    this.Name = this.RankNames[this.Index];
+    this.Image = this.RankImages[this.Index];
+	}
+
+   public string[] RankNames =
+   {
+      "2LT",
+      "1LT",
+      "CPT",
+      "MAJ",
+      "LTC",
+      "COL",
+      "BG",
+      "RADM",
+      "VADM",
+      "ADM",
+      "SADM",
+      "SD"
+    };
+
+    public Image[] RankImages =
+    {
+      Resources.DeckRank_1_2LT,
+      Resources.DeckRank_2_1LT,
+      Resources.DeckRank_3_CPT,
+      Resources.DeckRank_4_MAJ,
+      Resources.DeckRank_5_LTC,
+      Resources.DeckRank_6_COL,
+      Resources.DeckRank_7_BG,
+      Resources.DeckRank_8_RADM,
+      Resources.DeckRank_9_VADM,
+      Resources.DeckRank_10_ADM,
+      Resources.DeckRank_11_SADM,
+      Resources.DeckRank_12_SD
+    };
+}
--- Classes/DeckLeaderRankThresholds.cs
+++ Classes/DeckLeaderRankThresholds.cs
@@ -35,43 +35,12 @@ public class DeckLeaderRankThresholds
 }
 
 public class DeckLeaderRankThreshold {
-  public string[] RankNames =
-  {
-    "2LT",
-    "1LT",
-    "CPT",
-    "MAJ",
-    "LTC",
-    "COL",
-    "BG",
-    "RADM",
-    "VADM",
-    "ADM",
-    "SADM",
-    "SD"
-  };
+  public DeckLeaderRank Rank { get; }
 
-  public Image[] RankImages =
-  {
-    Resources.DeckRank_1_2LT,
-    Resources.DeckRank_2_1LT,
-    Resources.DeckRank_3_CPT,
-    Resources.DeckRank_4_MAJ,
-    Resources.DeckRank_5_LTC,
-    Resources.DeckRank_6_COL,
-    Resources.DeckRank_7_BG,
-    Resources.DeckRank_8_RADM,
-    Resources.DeckRank_9_VADM,
-    Resources.DeckRank_10_ADM,
-    Resources.DeckRank_11_SADM,
-    Resources.DeckRank_12_SD
-  };
   public DeckLeaderRankThreshold(byte rankIndex, byte[] byteData)
   {
     this.Bytes = byteData;
-    this.RankIndex = rankIndex;
-    this.RankName = RankNames[this.RankIndex];
-    this.RankImage = new Bitmap(RankImages[this.RankIndex], new Size(30, 30));
+    this.Rank = new DeckLeaderRank(rankIndex);
   }
 
   public void SetThresholdWithBytes(byte[] byteData)
@@ -110,7 +79,21 @@ public class DeckLeaderRankThreshold {
     }
   }
 
-  public ushort RankIndex { get; }
-  public string RankName{ get; }
-  public Image RankImage { get; }
+  public ushort RankIndex {
+    get {
+      return (ushort)this.Rank.Index; 
+    }
+  }
+  public string RankName{
+    get {
+      return this.Rank.Name;
+    }
+  }
+  public Image RankImage
+  {
+    get
+    {
+      return new Bitmap(this.Rank.Image, new Size(30, 30));
+    }
+  }
 }
